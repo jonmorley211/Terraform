54087b77145b127b9b298ba39e7a9bd159daace6
jonmorley211 <joncmorley@googlemail.com>
2022-01-12 15:37:13 +0000

    Update Main.cs

--- csharp/Main.cs
+++ csharp/Main.cs
@@ -21,8 +21,8 @@ namespace MyCompany.MyApp
 
             AwsProviderConfig config = new AwsProviderConfig();
             config.Region = "eu-west-2";
-            config.AccessKey = "AKIAULUYNPEQOA7MLHXC";
-            config.SecretKey = "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";
+            config.AccessKey = "";
+            config.SecretKey = "";
 
             //config.SharedCredentialsFile = "%USERPROFILE%/.aws/credentials/";
             Console.WriteLine("Region was " + config.Region);
